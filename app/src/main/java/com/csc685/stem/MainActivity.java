d5758e7ef055712d979cf23d8955c1ec1d88d51e
gitobu <allangitobu@hotmail.com>
2025-07-07 17:44:20 -0700

    Stem App Version 1.3

--- app/src/main/java/com/csc685/stem/MainActivity.java
+++ app/src/main/java/com/csc685/stem/MainActivity.java
@@ -1,6 +1,10 @@
 package com.csc685.stem;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,18 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        action();
+    }
+    private void action(){
+        Button button = findViewById(R.id.buttonSubmit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = findViewById(R.id.editTextWeight);
+                TextView textView = findViewById(R.id.textView);
+                int luggageWeight = Integer.parseInt(editText.getText().toString());
+            }
         });
     }
 }
